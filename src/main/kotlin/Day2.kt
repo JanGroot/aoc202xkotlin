e9a81928b8dbab2fe9c9f0c4f94618440e67e1be
Jan Groot <j.j.groot@gmail.com>
2020-12-02 12:07:49 +0100

    even more expressive

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,18 +1,15 @@
-data class Input(val under: Int, val upper: Int, val letter: Char, val password: String) {
+data class Password(val under: Int, val upper: Int, val letter: Char, val password: String) {
     fun passes() = password.count { it == letter } in under..upper
     fun passes2() = listOf(upper, under).count {password[it -1] == letter} == 1
 }
 
 fun main() {
-    val input = readLines("input2.txt").map {
+    val passwords = readLines("input2.txt").map {
         val match = """(\d+)-(\d+) (\w): (\w+)""".toRegex().find(it)
         val (under, upper, letter, password) = match!!.destructured
-        Input(under.toInt(), upper.toInt(), letter[0], password)
+        Password(under.toInt(), upper.toInt(), letter[0], password)
     }
-    println(part1(input))
-    println(part2(input))
+    println(passwords.count { it.passes() })
+    println(passwords.count { it.passes2() })
 }
 
-fun part1(input: List<Input>): Int = input.count { it.passes() }
-
-fun part2(input: List<Input>): Int = input.count { it.passes2() }
