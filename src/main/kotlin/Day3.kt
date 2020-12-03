0ecd6a1085dba870f03f0a64744e8e1225838951
Jan Groot <j.j.groot@gmail.com>
2020-12-03 09:40:15 +0100

    day 3
    them trees...

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val map = readLines("input3.txt")
+    println(map.getTreesForSlope(3))
+    println(map.getTreesForSlope(1) * map.getTreesForSlope(3) * map.getTreesForSlope(5) * map.getTreesForSlope(7) * map.getTreesForSlope(1,2))
+}
+
+fun List<String>.getTreesForSlope(right: Int, down: Int = 1): Int =
+    (down until size step down).count { (this[it][(right * (it / down)) % this[it].length] == '#')
+}
+
