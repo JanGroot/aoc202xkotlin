6c547e8450a27e4cc4dd9e0f70caef849a6607ba
Jan Groot <j.j.groot@gmail.com>
2020-12-05 01:01:41 +0100

    update to full regex

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,33 +1,24 @@
 fun main() {
     val records = {}.javaClass.getResource("input4.txt").readText().split("\n\n")
     val allPassports = records.map { record ->
-        record.split(" ", "\n").map { field ->
-            val (key, value) = field.split(":")
+        record.split("\\s".toRegex()).map {
+            val (key, value) = it.split(":")
             key to value
         }.toMap()
-    }.map { PassPort(it) }
+    }
     println(allPassports.count { it.hasRequiredFields() })
     println(allPassports.count { it.isValid() })
 }
 
-data class PassPort(val fields: Map<String, String>) {
-    fun hasRequiredFields() = fields.keys.containsAll(listOf("eyr", "hgt", "ecl", "byr", "iyr", "hcl", "pid"))
-    fun isValid() =
-        hasRequiredFields() && validBirthYear() && validIssueYear() && validExpireYear() && validHeight() && validHairColor() && validEyeColor() && validPassPortId()
+fun Map<String, String>.hasRequiredFields() = keys.containsAll(validations.keys)
+fun Map<String, String>.isValid() = validations.all { this[it.key]?.contains(it.value.toRegex()) ?: false }
 
-    private fun validBirthYear() = fields["byr"]!!.toInt() in 1920..2002
-    private fun validIssueYear() = fields["iyr"]!!.toInt() in 2010..2020
-    private fun validExpireYear() = fields["eyr"]!!.toInt() in 2020..2030
-    private fun validHeight(): Boolean {
-        val hgt = fields["hgt"]!!
-        return when {
-            (hgt.endsWith("cm")) -> hgt.substringBefore("cm").toInt() in 150..193
-            (hgt.endsWith("in")) -> hgt.substringBefore("in").toInt() in 59..76
-            else -> false
-        }
-    }
-
-    private fun validHairColor() = fields["hcl"]!!.matches("""^#[0-9a-fA-F]{6}$""".toRegex())
-    private fun validEyeColor() = fields["ecl"] in listOf("amb", "blu", "brn", "grn", "gry", "hzl", "oth")
-    private fun validPassPortId() = fields["pid"]!!.matches("""\d{9}""".toRegex())
-}
+private val validations = mapOf(
+    "eyr" to "(202\\d|2030)",
+    "hgt" to "((1[5-8]\\d|19[0-3])cm|^(59|6\\d|7[0-6])in)",
+    "ecl" to "(amb|blu|brn|grn|gry|hzl|oth)",
+    "byr" to "(19[2-9]\\d|200[0-2])",
+    "iyr" to "(201\\d|2020)",
+    "hcl" to "^#[0-9a-fA-F]{6}$",
+    "pid" to "^\\d{9}$"
+)
