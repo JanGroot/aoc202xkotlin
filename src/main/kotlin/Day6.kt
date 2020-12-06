870f35744de0c6da23acc589bb1622ced8322279
Jan Groot <j.j.groot@gmail.com>
2020-12-06 10:03:37 +0100

    day 6
    fun -> I was doing part 2 for part 1 :)

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val groups = {}.javaClass.getResource("input6.txt").readText().split("\n\n")
+    println(getUniqueAnswersPerGroup(groups).map { it.size }.sum())
+    println(getCommonAnswersPerGroup(groups).map { it.size }.sum())
+}
+
+fun getUniqueAnswersPerGroup(groups: List<String>) = groups.map { group -> group.lines().joinToString("").toSet() }
+
+fun getCommonAnswersPerGroup(groups: List<String>) =
+    groups.map { group -> group.lines().map { it.toSet() }.reduce { a, b -> a intersect b } }
+
+
+
+
