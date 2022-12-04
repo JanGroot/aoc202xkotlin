18df4e014d3257540997fdc9b2799235d3099b89
Jan Groot <j.j.groot@gmail.com>
2022-12-04 07:01:32 +0100

    day 4
    cleaned my initial gibberish up a bit

--- src/main/kotlin/twentytwentytwo/Day4.kt
+++ src/main/kotlin/twentytwentytwo/Day4.kt
@@ -1,19 +1,29 @@
 package twentytwentytwo
 
 fun main() {
-    val input = {}.javaClass.getResource("input-4.txt")!!.readText().split("\n");
+    val input = {}.javaClass.getResource("input-4.txt")!!.readText().split("\n").filter { it.isNotEmpty() }.map { s ->
+        s.split(",").map { p ->
+            p.split("-").map { it.toInt() }
+        }.map { it.first()..it.last() }
+    }
     val day = Day4(input)
     println(day.part1())
     println(day.part2())
 }
 
-class Day4(private val input: List<String>) {
+class Day4(private val input: List<List<IntRange>>) {
     fun part1(): Int {
-        error("not found")
+        return input
+            .count {
+                it.first().all { n -> it.last().contains(n) } || it.last().all { n -> it.first().contains(n) }
+            }
     }
 
     fun part2(): Int {
-        error("not found")
+        return input
+            .count {
+                it.first().any { n -> it.last().contains(n) } || it.last().any { n -> it.first().contains(n) }
+            }
     }
 }
 
