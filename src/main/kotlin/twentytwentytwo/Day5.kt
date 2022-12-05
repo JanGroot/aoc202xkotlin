8776ad42ce61ebc0d5483b44ac5ebcd545ec1bdf
Jan Groot <j.j.groot@gmail.com>
2022-12-05 12:01:26 +0100

    cleaned up
    removed the manual pre-parse.
    still not very happy as it's a bit of a mess.

--- src/main/kotlin/twentytwentytwo/Day5.kt
+++ src/main/kotlin/twentytwentytwo/Day5.kt
@@ -1,47 +1,61 @@
 package twentytwentytwo
 
 fun main() {
-    val input = {}.javaClass.getResource("input-5.txt")!!.readText().split("\n").filter { it.isNotEmpty() };
-    val day = Day5(input)
+    val (crates, moves) = {}.javaClass.getResource("input-5.txt")!!.readText().split("\n\n")
+    val day = Day5(crates, moves)
     println(day.part1())
     println(day.part2())
 }
 
-class Day5(private val input: List<String>) {
+class Day5(private val crates: String, private val moves: String) {
+
     fun part1(): String {
         val stacks = getStacks()
-        input.forEach {
-            val move = it.split(",").map{s -> s.toInt()}
-            for ( i in 1..move[0]) {
-                stacks[move[2]]!!.add(stacks[move[1]]!!.removeLast())
+        getMoves().forEach {
+            val (number, from, to) = it
+            for (i in 1..number) {
+                stacks[to]!!.add(stacks[from]!!.removeLast())
             }
         }
-        return stacks.values.map { it.last() }.reduce { code, s -> code + s  }
+        return stacks.values.map { it.last() }.reduce { code, s -> code + s }
     }
 
     fun part2(): String {
         val stacks = getStacks()
-        input.forEach {
-            val move = it.split(",").map{s -> s.toInt()}
-            val from = stacks[move[1]]
-            val first = from!!.take(from.size  - move[0])
-            val second = from.takeLast(move[0])
-            stacks[move[1]] = first.toMutableList()
-            stacks[move[2]]!!.addAll(second)
+        getMoves().forEach {
+            val (number, from, to) = it
+            val first = stacks[from]!!.take(stacks[from]!!.size - number)
+            val second = stacks[from]!!.takeLast(number)
+            stacks[from] = first.toMutableList()
+            stacks[to]!!.addAll(second)
         }
-        return stacks.values.map { it.last() }.reduce { code, s -> code + s  }
+        return stacks.values.map { it.last() }.reduce { code, s -> code + s }
     }
 
-    private fun getStacks() = mutableMapOf(
-        1 to arrayOf("H", "B", "V", "W", "N", "M", "L", "P").toMutableList(),
-        2 to arrayOf("M", "Q", "H").toMutableList(),
-        3 to arrayOf("N", "D", "B", "G", "F", "Q", "M", "L").toMutableList(),
-        4 to arrayOf("Z", "T", "F", "Q", "M", "W", "G").toMutableList(),
-        5 to arrayOf("M", "T", "H", "P").toMutableList(),
-        6 to arrayOf("C", "B", "M", "J", "D", "H", "G", "T").toMutableList(),
-        7 to arrayOf("M", "N", "B", "F", "V", "R").toMutableList(),
-        8 to arrayOf("P", "L", "H", "M", "R", "G", "S").toMutableList(),
-        9 to arrayOf("P", "D", "B", "C", "N").toMutableList()
-    )
+    private fun getStacks(): HashMap<Int, MutableList<String>> {
+        val stackedMap = crates.lines()
+            .filter { it.contains("[") }
+            .map { s ->
+                s.windowed(4, 4, true)
+                    .map { s -> s.trim() }
+                    .mapIndexed { index, value -> index + 1 to value }
+                    .filter { it.second.isNotEmpty() }
+            }.flatten()
+            .groupByTo(HashMap(), keySelector = { p -> p.first }, valueTransform = { p -> p.second[1].toString() })
+        stackedMap.values.forEach { it.reverse() }
+        return stackedMap
+    }
+
+    private fun getMoves(): List<List<Int>> {
+        val numRegex = "\\d+".toRegex()
+        return moves.split("\n")
+            .map { s ->
+                numRegex
+                    .findAll(s)
+                    .map { r -> r.value.trim().toInt() }
+                    .toList()
+            }
+            .filter { it.isNotEmpty() }
+    }
 }
 
