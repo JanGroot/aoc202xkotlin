1cc5f943ba6492bd4b612cd2743c217c33e007b3
Jan Groot <j.j.groot@gmail.com>
2022-12-08 11:16:00 +0100

    cache a bit for speed

--- src/main/kotlin/twentytwentytwo/Day8.kt
+++ src/main/kotlin/twentytwentytwo/Day8.kt
@@ -10,23 +10,29 @@ fun main() {
     println(day.part2())
 }
 
-class Day8(private val input: List<String>) {
+class Day8(input: List<String>) {
+    private val trees = input.mapIndexed { indexY, s ->
+        s.split("").filter { it.isNotEmpty() }.mapIndexed { indexX, s -> Pair(Point2d(indexX, indexY), s.toInt()) }
+    }.flatten()
+
+    private val rows = mutableMapOf<Int, List<Tree>>()
+    private val columns = mutableMapOf<Int, List<Tree>>()
+
+    init {
+        trees.forEach { (point) ->
+            rows.computeIfAbsent(point.y){ trees.filter { it.first.sameRow(point) } }
+            columns.computeIfAbsent(point.x){ trees.filter { it.first.sameColumn(point) } }
+        }
+    }
 
     fun part1(): Int {
-        val grid = input.mapIndexed { indexY, s ->
-            s.split("").filter { it.isNotEmpty() }.mapIndexed { indexX, s -> Pair(Point2d(indexX, indexY), s.toInt()) }
-        }.flatten();
-        val maxX = grid.maxOf { it.first.x }
-        val maxY = grid.maxOf { it.first.y }
-        return grid.filter { it.edge(maxX, maxY) || it.visible(grid) }.toSet().size
+        val maxX = trees.maxOf { it.first.x }
+        val maxY = trees.maxOf { it.first.y }
+        return trees.filter { it.edge(maxX, maxY) || it.visible()}.toSet().size
     }
 
     fun part2(): Int {
-        val grid = input.mapIndexed { indexY, s ->
-            s.split("").filter { it.isNotEmpty() }
-                .mapIndexed { indexX, s -> Tree(Point2d(indexX, indexY), s.toInt()) }
-        }.flatten();
-        return grid.maxOf { it.sees(grid) }
+        return trees.maxOf { it.sees() }
     }
 
     private fun List<Tree>.split(number: Int): Pair<List<Tree>, List<Tree>> {
@@ -34,11 +40,11 @@ class Day8(private val input: List<String>) {
     }
 
 
-    fun Tree.sees(grid: List<Tree>): Int {
-        val row = grid.filter { this.first.sameRow(it.first) }
-        val column = grid.filter { this.first.sameColumn(it.first) }
-        val (left, right) = row.split(this.first.x)
-        val (above, under) = column.split(this.first.y)
+    fun Tree.sees(): Int {
+        val row = rows[first.y]
+        val column = columns[first.x]
+        val (left, right) = row!!.split(this.first.x)
+        val (above, under) = column!!.split(this.first.y)
         return left.reversed().takeWhile { it.second < second }.count()
             .let { count -> if (count == left.size) count else count + 1 } * right.takeWhile { it.second < second }
             .count().let { count -> if (count == right.size) count else count + 1 } * above.reversed()
@@ -47,11 +53,11 @@ class Day8(private val input: List<String>) {
             .count().let { count -> if (count == under.size) count else count + 1 }
     }
 
-    fun Pair<Point2d, Int>.visible(grid: List<Tree>) : Boolean{
-        val row = grid.filter { this.first.sameRow(it.first) }
-        val column = grid.filter { this.first.sameColumn(it.first) }
-        val (left, right) = row.split(this.first.x)
-        val (above, under) = column.split(this.first.y)
+    fun Tree.visible() : Boolean{
+        val row = rows[first.y]
+        val column = columns[first.x]
+        val (left, right) = row!!.split(this.first.x)
+        val (above, under) = column!!.split(this.first.y)
         return left.all {it.second < second}  || right.all{it.second < second} || above.all{it.second < second} || under.all { it.second < second }
     }
 
