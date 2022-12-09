8cfbdeca92bab71346d7461c33c1a7f0f5ec3b0f
Jan Groot <j.j.groot@gmail.com>
2022-12-09 07:12:28 +0100

    follow the leader!

--- src/main/kotlin/twentytwentytwo/Day9.kt
+++ src/main/kotlin/twentytwentytwo/Day9.kt
@@ -1,19 +1,61 @@
 package twentytwentytwo
 
+import java.awt.Point
+
 fun main() {
     val input = {}.javaClass.getResource("input-9.txt")!!.readText().linesFiltered { it.isNotEmpty() };
-    val day = DayX(input)
+    val day = Day9(input)
     println(day.part1())
     println(day.part2())
 }
 
 class Day9(private val input: List<String>) {
     fun part1(): Int {
-        error("not found")
+    var visited = mutableSetOf<Structures.Point2d>()
+        var head = Structures.Point2d(0, 0)
+        var tail1 = Structures.Point2d(0, 0)
+        input.forEach {
+            val (dir, steps) = it.split(" ")
+            repeat(steps.toInt()) {
+                head = head.move(dir)
+                tail1 = tail1.follow(head)
+                visited.add(tail1)
+            }
+        }
+        return visited.size
     }
 
     fun part2(): Int {
-        error("not found")
+        var visited = mutableSetOf<Structures.Point2d>()
+        var head = Structures.Point2d(0, 0)
+        var tail1 = Structures.Point2d(0, 0)
+        var tail2 = Structures.Point2d(0, 0)
+        var tail3 = Structures.Point2d(0, 0)
+        var tail4 = Structures.Point2d(0, 0)
+        var tail5 = Structures.Point2d(0, 0)
+        var tail6 = Structures.Point2d(0, 0)
+        var tail7 = Structures.Point2d(0, 0)
+        var tail8 = Structures.Point2d(0, 0)
+        var tail9 = Structures.Point2d(0, 0)
+        input.forEach {
+            val (dir, steps) = it.split(" ")
+            repeat(steps.toInt()) {
+                head = head.move(dir)
+                tail1 = tail1.follow(head)
+                tail2 = tail2.follow(tail1)
+                tail3 = tail3.follow(tail2)
+                tail4 = tail4.follow(tail3)
+                tail5 = tail5.follow(tail4)
+                tail6 = tail6.follow(tail5)
+                tail7 = tail7.follow(tail6)
+                tail8 = tail8.follow(tail7)
+                tail9 = tail9.follow(tail8)
+                visited.add(tail9)
+            }
+
+
+        }
+        return visited.size
     }
 }
 
--- src/main/kotlin/twentytwentytwo/Structures.kt
+++ src/main/kotlin/twentytwentytwo/Structures.kt
@@ -64,10 +64,60 @@ class Structures {
 
     data class Point2d(val x: Int, val y: Int) {
 
-        infix fun sameRow(that: Point2d): Boolean  =
+        infix fun sameRow(that: Point2d): Boolean =
             y == that.y
 
         infix fun sameColumn(that: Point2d): Boolean =
             x == that.x
+
+        fun neighbors(): List<Point2d> =
+            listOf(
+                Point2d(x, y + 1),
+                Point2d(x, y - 1),
+                Point2d(x + 1, y),
+                Point2d(x - 1, y)
+            )
+
+        fun allNeighbors(): List<Point2d> =
+            neighbors() + listOf(
+                Point2d(x - 1, y - 1),
+                Point2d(x - 1, y + 1),
+                Point2d(x + 1, y - 1),
+                Point2d(x + 1, y + 1)
+            )
+
+        fun move(dir: String): Point2d {
+            return when (dir) {
+                "U" -> Point2d(x, y + 1)
+                "D" -> Point2d(x, y - 1)
+                "L" -> Point2d(x - 1, y)
+                "R" -> Point2d(x + 1, y)
+                else -> {
+                    this
+                }
+            }
+        }
+
+        fun follow(other: Point2d): Point2d {
+            when {
+                other == this -> return this
+                other in allNeighbors() -> return this
+                sameRow(other) -> {
+                   return  if (other.x > x) move("R") else move("L")
+
+                }
+
+                sameColumn(other) -> {
+                    return if (other.y > y) move("U") else move("D")
+
+                }
+
+                else -> {
+                    val step1 = if (other.x > x) move("R") else move("L")
+                    return if (other.y > step1.y) step1.move("U") else step1.move("D")
+                }
+
+            }
+        }
     }
 }
