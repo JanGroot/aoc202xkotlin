3220ff68fdfb0590db7fc3100b021da57c8058b2
Jan Groot <j.j.groot@gmail.com>
2022-12-10 06:51:21 +0100

    it's all on the tv :)

---
+++ src/main/kotlin/twentytwentytwo/Day10.kt
@@ -0,0 +1,74 @@
+package twentytwentytwo
+
+fun main() {
+    val input = {}.javaClass.getResource("input-10.txt")!!.readText().linesFiltered { it.isNotEmpty() };
+    val day = Day10(input)
+    println(day.part1())
+    println(day.part2())
+}
+
+class Day10(private val input: List<String>) {
+    fun part1(): Int {
+        var cycle = 0
+        var value = 1
+        var result = mutableListOf<Int>()
+        input.forEach {
+            when (it) {
+                "noop" -> {
+                    if (++cycle in intArrayOf(20, 60, 100, 140, 180, 220)) {
+                        result.add(value * cycle)
+                    }
+                }
+
+                else -> {
+                    if (++cycle in intArrayOf(20, 60, 100, 140, 180, 220)) {
+                        result.add(value * cycle)
+                    }
+                    if (++cycle in intArrayOf(20, 60, 100, 140, 180, 220)) {
+                        result.add(value * cycle)
+                    }
+                    value += it.split(" ")[1].toInt()
+                }
+
+            }
+        }
+        return result.sum()
+    }
+
+    fun part2(): Int {
+        val lines = intArrayOf(40, 80, 120, 160, 200, 240)
+        var cycle = 0
+        var value = 1
+        var row = ""
+        input.forEach {
+            when (it) {
+                "noop" -> {
+                    cycle++
+                    row += if (cycle % 40 in value -1..value +1) "#" else "."
+                    if (cycle in lines) {
+                        println(row)
+                        row = ""
+                    }
+                }
+                else -> {
+                    cycle++
+                    row += if (cycle % 40 in value-1..value +1) "#" else "."
+                    if (cycle in lines) {
+                        println(row)
+                        row = ""
+                    }
+                    value += it.split(" ")[1].toInt()
+                    cycle++
+                    row += if (cycle % 40  in value-1..value +1) "#" else "."
+                    if (cycle in lines) {
+                        println(row)
+                        row = ""
+                    }
+                }
+
+            }
+        }
+        return 0
+    }
+}
+
