8d3da4d894edcf0a5436e8bbd2c14b4832f0829f
Jan Groot <j.j.groot@gmail.com>
2022-12-16 13:48:01 +0100

    pulling in a graph class lib from blog: ajavageek/dijkstra
    example waas with string as node, made it generic

---
+++ src/main/kotlin/twentytwentytwo/Graph.kt
@@ -0,0 +1,47 @@
+package twentytwentytwo
+
+class Graph<T>(private val weightedPaths: Map<T, Map<T, Int>>) {
+
+    fun findShortestPath(start: T, end: T): Int {
+        val paths = recurseFindShortestPath(NodePaths(start, end)).paths
+        return paths.getValue(end)
+    }
+
+    private tailrec fun recurseFindShortestPath(nodePaths: NodePaths<T>): NodePaths<T> {
+        return if (nodePaths.isFinished()) nodePaths
+        else {
+            val nextNode = nodePaths.nextNode(weightedPaths)
+            recurseFindShortestPath(nextNode)
+        }
+    }
+}
+
+data class NodePaths<T>(private val node: T, private  val end: T, val paths: Map<T, Int> = emptyMap()) {
+
+    private fun updatePath(entry: Map.Entry<T, Int>): NodePaths<T> {
+        val currentDistance = paths.getOrDefault(entry.key, Integer.MAX_VALUE)
+        val newDistance = entry.value + paths.getOrDefault(node, 0)
+        return if (newDistance < currentDistance)
+            this + (entry.key to newDistance)
+        else
+            this
+    }
+
+    private fun updatePaths(weightedPaths: Map<T, Map<T, Int>>) = (weightedPaths[node]
+        ?.map { updatePath(it) }
+        ?.fold(copy()) { acc, item -> acc + item } ?: copy()) - node
+
+    fun nextNode(weightedPaths: Map<T, Map<T, Int>>): NodePaths<T> {
+        val updatedPaths = updatePaths(weightedPaths)
+        val nextNode = updatedPaths.paths.minBy { it.value }.key
+        return updatedPaths.copy(node = nextNode)
+    }
+
+    fun isFinished() = node == end
+
+    operator fun plus(other: NodePaths<T>) = copy(paths = paths + other.paths)
+
+    operator fun plus(pair: Pair<T, Int>) = copy(paths = paths + pair)
+
+    operator fun minus(node: T)= copy(paths = paths - node)
+}
