3dd7866026904356348c41a74ce2141262f7603a
Jan Groot <j.j.groot@gmail.com>
2022-12-16 23:30:41 +0100

    I need to sleep haha fail after fail.

--- src/main/kotlin/twentytwentytwo/Day16.kt
+++ src/main/kotlin/twentytwentytwo/Day16.kt
@@ -1,21 +1,21 @@
 package twentytwentytwo
 
+import java.lang.Integer.max
+
 fun main() {
     val input = {}.javaClass.getResource("input-16.txt")!!.readText().linesFiltered { it.isNotEmpty() };
     val testInput = {}.javaClass.getResource("input-16-1.txt")!!.readText().linesFiltered { it.isNotEmpty() };
     val test = Day16(testInput)
     val day = Day16(input)
     println(test.part1())
-  //  println(test.part2())
+    println(test.part2())
     println(day.part1())
-   // println(day.part2())
+    println(day.part2())
 }
 
 class Day16(input: List<String>) {
 
-    private val graph: Graph<Valve>
     private val valves: Map<String, Valve>
-    private val valveMapMap:  Map<Valve, Map<Valve, Int>>
     private val paths: Map<Valve, Map<Valve, Int>>
 
     init {
@@ -24,20 +24,43 @@ class Day16(input: List<String>) {
             val rate = it.substringAfter("=").substringBefore(";").toInt()
             name to Valve(name, rate = rate)
         }
-        valveMapMap = input.associate {
+        paths = Graph(input.associate {
             val name = it.substringAfter("Valve ").substringBefore(" has")
             val edges = it.substringAfter("to valve").split(", ")
             valves[name]!! to edges.associate { valves[it.filter { it.isUpperCase() }]!! to 1 }
-        }
-        graph = Graph(valveMapMap)
-        paths = graph.findAllShortestPath()
+        }).findAllShortestPath().map { (k, v) -> k to v.filter { it.key.rate > 0 } }.toMap()
     }
+
     fun part1(): Int {
-        return 0;
+        val volcano = Volcano(paths, valves["AA"]!!)
+        volcano.findHighestDeltaP()
+        return volcano.pressure
     }
 
     fun part2(): Int {
-        error("not found")
+        val volcano = Volcano(paths, valves["AA"]!!, 26)
+        volcano.findHighestDeltaP(secondPass = true)
+        return volcano.pressure
+    }
+}
+
+class Volcano(private val paths: Map<Valve, Map<Valve, Int>>, private val start: Valve, private val timeToBlow: Int = 30) {
+    var pressure = 0
+    fun findHighestDeltaP(from: Valve = start, cache: Set<Valve> = mutableSetOf(), deltaT: Int = 0, deltaP: Int = 0, secondPass: Boolean = false) {
+        pressure = max(pressure, deltaP)
+        paths[from]!!
+            .filter { (destination, distance) ->
+            (!cache.contains(destination) && deltaT + distance + 1 < timeToBlow) }
+            .forEach() { (destination, distance) ->
+                findHighestDeltaP(
+                    from = destination,
+                    cache = cache + destination,
+                    deltaT = deltaT + 1 + distance,
+                    deltaP = deltaP + (timeToBlow - deltaT - distance - 1) * destination.rate,
+                    secondPass
+                )
+            }
+        if (secondPass) findHighestDeltaP(start, cache, deltaT = 0, deltaP)
     }
 }
 
