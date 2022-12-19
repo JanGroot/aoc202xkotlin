d2d3bdf09f1763e3c746249b14f817c807177469
Jan Groot <j.j.groot@gmail.com>
2022-12-19 12:11:20 +0100

    suspect cache key is not working

--- src/main/kotlin/twentytwentytwo/Day19.kt
+++ src/main/kotlin/twentytwentytwo/Day19.kt
@@ -1,6 +1,8 @@
 package twentytwentytwo
 
 import java.lang.Integer.max
+import java.util.Collections.max as colMax
+
 
 fun main() {
     val input = {}.javaClass.getResource("input-19.txt")!!.readText().linesFiltered { it.isNotEmpty() };
@@ -22,12 +24,32 @@ class Day19(private val input: List<String>) {
         val clay = Robot("clay", mapOf("ore" to split[3].toInt()))
         val obsidian = Robot("obsidian", mapOf("ore" to split[5].toInt(), "clay" to split[6].toInt()))
         val geode = Robot("geode", mapOf("ore" to split[8].toInt(), "obsidian" to split[9].toInt()))
-        Blueprint(id, mapOf(ore, clay, obsidian, geode))
+        val oreLimit = colMax(
+            listOf(
+                ore.second["ore"] ?: 0, clay.second["ore"] ?: 0, obsidian.second["ore"] ?: 0, geode.second["ore"] ?: 0
+            )
+        )
+        val clayLimit = colMax(
+            listOf(
+                ore.second["clay"] ?: 0,
+                clay.second["clay"] ?: 0,
+                obsidian.second["clay"] ?: 0,
+                geode.second["clay"] ?: 0
+            )
+        )
+        val obsidianLimit = colMax(
+            listOf(
+                ore.second["obsidian"] ?: 0,
+                clay.second["obsidian"] ?: 0,
+                obsidian.second["obsidian"] ?: 0,
+                geode.second["obsidian"] ?: 0
+            )
+        )
+        Blueprint(id, mapOf(ore, clay, obsidian, geode), oreLimit, clayLimit, obsidianLimit)
     }
 
     fun part1(): Int {
         return blueprints.maxOf { it.maxGeodes() }
-        error("not found")
     }
 
     fun part2(): Int {
@@ -35,36 +57,40 @@ class Day19(private val input: List<String>) {
     }
 }
 
-class Blueprint(val id: Int, val robots: Map<String, Map<String, Int>>) {
+class Blueprint(
+    val id: Int,
+    private val robots: Map<String, Map<String, Int>>,
+    private val oreLimit: Int,
+    private val clayLimit: Int,
+    private val obsidianLimit: Int
+) {
 
     val cache = mutableSetOf<State>()
-    var geodeMax = 0
 
     fun maxGeodes(): Int {
-        step((mapOf("ore" to 1) to (emptyMap<String, Int>())) to 1)
-        return maxGeodes();
+        return step((mapOf("ore" to 1) to (emptyMap<String, Int>())) to 1)
     }
 
-    fun step(state: State) {
-        if (state.getStep() == 24) return
-        if (state !in cache) {
-            geodeMax = max(geodeMax, state.inventory()["geode"] ?: 0)
+    fun step(state: State): Int {
+        if (state.getStep() == 24) return 0
+        return if (state !in cache) {
             cache.add(state)
             val produced = produce(state.producers(), state.inventory())
-            buyRobots((state.producers() to produced) to state.getStep())
-            step((state.producers() to produced) to state.getStep() + 1)
-
+            val max = step((state.producers() to produced) to state.getStep() + 1)
+            max(max, buyRobots((state.producers() to produced) to state.getStep()))
         } else {
-            geodeMax = max(geodeMax, state.inventory()["geode"] ?: 0)
+            state.inventory()["geode"] ?: 0
         }
     }
 
-    private fun buyRobots(state: State) {
+    private fun buyRobots(state: State): Int {
+        var max = 0
         robots.keys.forEach {
             if (canBuyRobot(it, state.inventory()) && shouldBuyRobot(it, state.producers()[it] ?: 0)) {
-                step(buyRobot(it, state))
+                max = max(max, step(buyRobot(it, state)))
             }
         }
+        return max
     }
 
     private fun buyRobot(robot: String, state: State): State {
@@ -75,20 +101,29 @@ class Blueprint(val id: Int, val robots: Map<String, Map<String, Int>>) {
         return ((state.producers() + (robot to robots) to state.inventory() + costs) to state.getStep() + 1)
     }
 
-    private fun canBuyRobot(robot: String, inventory: Map<String, Int>): Boolean =
-        robots[robot]!!.all {
-            ( inventory[it.key] ?: 0 )>= it.value
+    private fun canBuyRobot(robot: String, inventory: Map<String, Int>): Boolean = robots[robot]!!.all {
+        (inventory[it.key] ?: 0) >= it.value
+    }
+
+    private fun shouldBuyRobot(robot: String, number: Int): Boolean = when (robot) {
+        "geode" -> true
+        "ore" -> number < oreLimit
+        "obsidian" -> number < obsidianLimit
+        "clay" -> number < clayLimit
+        else -> {
+            error("oops")
         }
-
-    private fun shouldBuyRobot(robot: String, number: Int) =
-        (robot == "geode") || robots.filterValues { it.containsKey(robot) }.values.all { it[robot]!! > number }
+    }
 
 
-    private fun produce(producers: Map<String, Int>, inventory: Map<String, Int>): Map<String, Int> = producers.map {
+    private fun produce(
+        producers: Map<String, Int>, inventory: Map<String, Int>
+    ): Map<String, Int> = producers.map {
         it.key to inventory.getOrDefault(it.key, 0) + it.value
     }.toMap()
 
 }
+
 
 fun State.producers() = first.first
 fun State.inventory() = first.second
