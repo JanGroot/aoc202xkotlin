bf07912e714551f4ed4305c8e75ce0c905ccf5af
Jan Groot <j.j.groot@gmail.com>
2022-12-19 06:31:27 +0100

    30 minutes in, parsing done haha.. I'm so slow.

--- src/main/kotlin/twentytwentytwo/Day19.kt
+++ src/main/kotlin/twentytwentytwo/Day19.kt
@@ -12,6 +12,17 @@ fun main() {
 }
 
 class Day19(private val input: List<String>) {
+    val blueprints  = input.mapIndexed { index, s ->
+        val id = index + 1
+        val split = s.split("robot costs ", " ore. Each ", " ore and ", " clay. Each geode ", " obsidian.")
+        println(split)
+        val ore = Robot("ore", mutableListOf("ore" to split[1].toInt()))
+        val clay = Robot("clay", mutableListOf("ore" to split[3].toInt()))
+        val obsidian = Robot("obsidian", mutableListOf("ore" to split[5].toInt(), "clay" to split[6].toInt()))
+        val geode = Robot("geode", mutableListOf("ore" to split[8].toInt(), "obsidian" to split[9].toInt()))
+        Blueprint(id, listOf(ore,clay,obsidian,geode))
+    }.also { println(it) }
+
     fun part1(): Int {
         error("not found")
     }
@@ -20,4 +31,8 @@ class Day19(private val input: List<String>) {
         error("not found")
     }
 }
+
+data class Blueprint(val id: Int,val robots: List<Robot>)
+data class Robot(val type: String, val costs: List<Cost>)
+typealias Cost = Pair<String,Int>
 
