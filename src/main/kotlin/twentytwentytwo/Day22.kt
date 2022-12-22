5867bdbe748776f754bedd1be905836c701b21fb
Jan Groot <j.j.groot@gmail.com>
2022-12-22 09:03:59 +0100

    wip

--- src/main/kotlin/twentytwentytwo/Day22.kt
+++ src/main/kotlin/twentytwentytwo/Day22.kt
@@ -2,10 +2,10 @@ package twentytwentytwo
 
 
 fun main() {
-    val input = {}.javaClass.getResource("input-22.txt")!!.readText().linesFiltered { it.isNotEmpty() };
-    val tinput = {}.javaClass.getResource("input-22-1.txt")!!.readText().linesFiltered { it.isNotEmpty() };
-    val test = Day21(tinput)
-    val day = Day21(input)
+    val input = {}.javaClass.getResource("input-22.txt")!!.readText().split("\n\n");
+    val tinput = {}.javaClass.getResource("input-22-1.txt")!!.readText().split("\n\n");
+    val test = Day22(tinput)
+    val day = Day22(input)
     println(test.part1())
     println(test.part2())
     println(day.part1())
@@ -13,14 +13,58 @@ fun main() {
 }
 
 
-class Day22(input: List<String>) {
+class Day22(val input: List<String>) {
+    val land = input[0].lines().mapIndexed { y, row ->
+        row.mapIndexed { x, s -> x to y to s }
+    }.flatten().filter { p -> p.second != ' ' }
+    val path = "([R|L])|(\\d+)".toRegex().findAll(input[1]).map { it.value }.toList()
+
 
     fun part1() {
-        TODO()
+        var position = land.first().first
+        println(land)
+        println(path)
+        path.forEach { step ->
+            when (step) {
+                "R" -> rotate("R")
+                "L" -> rotate("R")
+                else -> {
+                    position = move(position, step.toInt())
+                }
+            }
+
+            println(position)
+        }
+
+
     }
 
     fun part2() {
         TODO()
     }
-}
 
+    /*    fun Structures.Point2d.move(steps: Int) =
+            if (steps == 0) this
+            else {
+                if (land.contains(move(direction))
+            }*/
+    companion object {
+        var currentDirection = "R"
+        val directions = arrayOf("U", "R", "D", "L")
+        fun rotate(newDirection: String): Unit {
+            when (newDirection) {
+                "R" -> currentDirection = directions[(directions.indexOf(currentDirection) + 1).mod(4)]
+                "L" -> currentDirection = directions[(directions.indexOf(currentDirection) + 1).mod(4)]
+                else -> {
+                    error("Cant move there")
+                }
+            }
+        }
+
+        fun move(position:Pair<Int,Int>, steps: Int): Pair<Int,Int> {
+            if (steps == 0) return position
+            var next = Structures.Point2d(position.first, position.second).move(currentDirection)
+            return move(next.x to next.y, steps -1)
+        }
+    }
+}
